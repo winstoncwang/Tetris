7aa7f4857baaa4dbd8cb0a9d5f976946be0b873c
winstoncwang <congwang@live.co.uk>
2020-06-16 13:13:07 +0100

    2.7
    setup webpack, config files, npm setup and import/export

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+let path = require('path');
+
+module.exports = {
+	entry  : './src/app.js',
+	output : {
+		path     : path.resolve(__dirname, './dist'),
+		filename : 'bundle.js '
+	},
+	module : {
+		rules : [
+			{
+				test : /\.css$/,
+				use  : [ { loader: 'style-loader' }, { loader: 'css-loader' } ]
+			}
+		]
+	}
+};
